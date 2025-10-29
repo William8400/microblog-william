b7288b70c72263259a213e6e3e0df686e1356736
William8400 <wdonato02@gmail.com>
2025-10-29 17:19:34 -0300

    .

--- src/Helpers/Utils.php
+++ src/Helpers/Utils.php
@@ -5,7 +5,7 @@ class Utils {
 
     /* Usamos Mixed para sinalizar que o método aceita/retorna tipos de dados variados (string, int, array, float etc) */
 
-    // Quando coloca static você não precisa fazer um objeto
+    // Quando coloca static você não precisa fazer um objeto você pode usar direto
     public static function sanitizar(mixed $valor, string $tipoDeSanitizacao = 'texto'):mixed {
         switch($tipoDeSanitizacao){
             
@@ -25,7 +25,7 @@ class Utils {
         return password_hash($valorSenha, PASSWORD_DEFAULT);
     }
 
-    public static function dump($dados):void{
+    public static function dump(mixed $dados):void{
         echo '<pre>';
         var_dump($dados);
         echo '</pre>';
