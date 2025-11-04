35af37c32220e772590ce3edd385b5fc9dfe50de
William8400 <wdonato02@gmail.com>
2025-11-04 17:27:20 -0300

    Update usuarios.php

--- admin/usuarios.php
+++ admin/usuarios.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once "../src/Database/Conecta.php";
 require_once "../src/Services/UsuarioServico.php";
 require_once "../src/Helpers/Utils.php";
@@ -11,9 +11,9 @@ $usuariosServico = new UsuarioServico();
 
 try {
 	$usuarios = $usuariosServico->buscar();
-    // Utils::dump($usuarios);
+	// Utils::dump($usuarios);
 } catch (Throwable $e) {
-	$erro = "Erro ao buscar usuários. <br>".$e->getMessage();
+	$erro = "Erro ao buscar usuários. <br>" . $e->getMessage();
 }
 
 
@@ -25,7 +25,7 @@ require_once "../includes/cabecalho-admin.php";
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
-		
+
 		<h2 class="text-center">Usuários <span class="badge bg-dark">X</span></h2>
 
 		<?php if ($erro): ?>
@@ -34,12 +34,12 @@ require_once "../includes/cabecalho-admin.php";
 
 		<p class="text-center mt-5">
 			<a class="btn btn-primary" href="usuario-insere.php">
-			<i class="bi bi-plus-circle"></i>	
-			Inserir novo usuário</a>
+				<i class="bi bi-plus-circle"></i>
+				Inserir novo usuário</a>
 		</p>
-				
+
 		<div class="table-responsive">
-		
+
 			<table class="table table-hover">
 				<thead class="table-light">
 					<tr>
@@ -52,34 +52,39 @@ require_once "../includes/cabecalho-admin.php";
 
 				<tbody>
 
-				
-					<tr>
-						<td> nome do usuário... </td>
-						<td> email do usuário... </td>
-						<td> tipo do usuário... </td>
-						<td class="text-center">
-							<a class="btn btn-warning" 
-							href="usuario-atualiza.php">
-							<i class="bi bi-pencil"></i> Atualizar
-							</a>
-						
-							<a class="btn btn-danger excluir" 
-							href="usuario-exclui.php">
-							<i class="bi bi-trash"></i> Excluir
-							</a>
-						</td>
-					</tr>
-				
+					<?php foreach ($usuarios as  $usuario) { ?>
 
-				</tbody>                
+
+						<tr>
+							<td><?= $usuario['nome'] ?></td>
+							<td><?= $usuario['email'] ?></td>
+							<td><?= $usuario['tipo'] ?></td>
+							<td class="text-center">
+								<a class="btn btn-warning"
+									href="usuario-atualiza.php?id=<?=$usuario['id']?>">
+									<i class="bi bi-pencil"></i> Atualizar
+								</a>
+
+								<a class="btn btn-danger excluir"
+									href="usuario-exclui.php?id=<?=$usuario['id']?>">
+									<i class="bi bi-trash"></i> Excluir
+								</a>
+							</td>
+						</tr>
+
+
+					<?php } ?>
+
+
+
+				</tbody>
 			</table>
-	</div>
-		
+		</div>
+
 	</article>
 </div>
 
 
-<?php 
+<?php
 require_once "../includes/rodape-admin.php";
 ?>
-
