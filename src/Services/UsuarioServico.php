7bc9b0bbd30c7e0247cb7820da5bf971c1a16b38
William8400 <wdonato02@gmail.com>
2025-11-05 14:32:24 -0300

    Função buscarPorId

--- src/Services/UsuarioServico.php
+++ src/Services/UsuarioServico.php
@@ -40,4 +40,23 @@ class UsuarioServico {
         $consulta = $this->conexao->query($sql);
         return $consulta->fetchAll();
     }
-}
+
+    // buscarPorId (SELECT/WHERE)
+
+    public function buscarPorId(int $valorId):?array {
+        
+        $sql = "SELECT * FROM usuarios WHERE id = :id";
+
+        $consulta = $this->conexao->prepare($sql);
+
+        $consulta->bindValue(":id", $valorId);
+
+        /* Sobre o  ?: conhecido como "Elvis Operator"  
+        
+        É uma condicional simplificada/abreviada em que, 
+        se a condição/expressão for válida (ou seja, tem dados),
+        ela mesma é retornada. Caso contrário, é retornado null  */
+        $consulta->execute();
+        return $consulta->fetch() ?: null;
+    }
+    }
