e9cd0c87ce1fee1d4e9100c4b4a9c34522cedd26
William8400 <wdonato02@gmail.com>
2025-11-11 13:38:32 -0300

    -

--- admin/usuario-exclui.php
+++ admin/usuario-exclui.php
@@ -12,31 +12,17 @@ $erro = null;
 
 $usuarioServico = new UsuarioServico();
 
-try {
-	$dados = $usuarioServico->buscarPorId($id);
-	if (!$dados) $erro = "Usuário não encontrado";
-} catch (Throwable $e) {
-	$erro = "Erro ao buscar usuário. <br>" . $e->getMessage();
-}
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	
 	try {
 		
-		$id = Utils::sanitizar($_POST['id'], 'int');
-
-		$Usuario = new Usuario("","","","");
-
-		$usuario->setId($id);
-
-		$usuarioServico->excluirUsuario($usuario);
+		$usuarioServico->excluirUsuario($id);
 
 		Utils::redirecionarPara('usuarios.php');
 
 	} catch (Throwable $e) {
 		$erro = "Erro ao excluir usuário. <br>" . $e->getMessage();
 	}
-}
+
 
 require_once "../includes/cabecalho-admin.php";
 ?>
@@ -51,39 +37,9 @@ require_once "../includes/cabecalho-admin.php";
 
 		<?php if ($erro): ?>
 			<p class="alert alert-danger text-center"> <?= $erro ?> </p>
+		<?php else: ?>
+		  <p>Excluido com sucesso!</p>
 		<?php endif; ?>
-
-		<form class="mx-auto w-75" action="" method="post" id="form-excluir" name="form-excluir" autocomplete="off">
-			<input type="hidden" name="id" value="<?= $dados['id'] ?>">
-
-			<div class="mb-3">
-				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" value="<?= $dados['nome'] ?>">
-			</div>
-
-			<div class="mb-3">
-				<label class="form-label" for="email">E-mail:</label>
-				<input class="form-control" type="email" id="email" name="email" value="<?= $dados['email'] ?>">
-			</div>
-
-			<div class="mb-3">
-				<label class="form-label" for="senha">Senha:</label>
-				<input class="form-control" type="password" id="senha" name="senha" placeholder="Preencha apenas se for alterar">
-			</div>
-
-			<div class="mb-3">
-				<label class="form-label" for="tipo">Tipo:</label>
-				<select class="form-select" name="tipo" id="tipo">
-					<option value=""></option>
-
-					<option value="editor" <?php if ($dados['tipo'] === 'editor') echo "selected" ?>>Editor</option>
-
-					<option value="admin" <?php if ($dados['tipo'] === 'admin') echo "selected" ?>>Administrador</option>
-				</select>
-			</div>
-
-			<button class="btn btn-primary" name="excluir"><i class="bi bi-arrow-clockwise"></i> Excluir Usuario</button>
-		</form>
 </article>
 </div>
 
--- src/Services/UsuarioServico.php
+++ src/Services/UsuarioServico.php
@@ -80,13 +80,13 @@ class UsuarioServico {
         $consulta->execute();
     }
 
-    public function excluirUsuario(Usuario $dadosDoUsuario):void {
+    public function excluirUsuario(int $id):void {
         
         $sql = "DELETE FROM usuarios WHERE id = :id";
 
         $consulta = $this->conexao->prepare($sql);
 
-        $consulta->bindValue(":id", $dadosDoUsuario->getId());
+        $consulta->bindValue(":id", $id);
 
         $consulta->execute();
     }
