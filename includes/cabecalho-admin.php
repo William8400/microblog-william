90e251b3060d866c4e183be90782d49d30809d26
William8400 <wdonato02@gmail.com>
2025-11-12 17:11:01 -0300

    -

--- includes/cabecalho-admin.php
+++ includes/cabecalho-admin.php
@@ -51,11 +51,13 @@ if (isset($_GET['sair'])) {
                         </li>
 
 
-                       
+                       <?php if ($_SESSION['tipo'] == 'admin'): ?>
+                        
                         <li class="nav-item">
                             <a class="nav-link" href="usuarios.php">Usuários</a>
                         </li>
-
+                          
+                        <?php endif; ?>
 
                         <li class="nav-item">
                             <a class="nav-link" href="../index.php" target="_blank">Área pública</a>
