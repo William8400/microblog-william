30d2c303dc1a18581156e4b2a6eff931b78ca15d
William8400 <wdonato02@gmail.com>
2025-11-12 14:27:05 -0300

    Comentário de Capturar e-mail e senha

--- login.php
+++ login.php
@@ -10,7 +10,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (empty($_POST['email']) || empty($_POST['senha'])) {
         Utils::redirecionarPara("login.php?campos_obrigatorios");
     } else {
-        echo "Tudo preenchido!";
+       // Captura e-mail e senha 
+
+       // Busca pelo usuário através do e-mail
+
+       // Se não existir usuário/usuário inválido, redirecione para login
+
+       // Caso contrário: virifique a senha
+       // Estando correta, faça o login 
+       // Estando errada, mantenha em login.php 
     }
 }
 
