5d93ba2244814dec139839bc1cf46dadf759f293
William8400 <wdonato02@gmail.com>
2025-11-18 17:07:55 -0300

    Update NoticiaServico.php

--- src/Services/NoticiaServico.php
+++ src/Services/NoticiaServico.php
@@ -12,7 +12,16 @@ class NoticiaServico {
     // Versão Básica (provisória)
     public function buscar():array {
         
-        $sql = "SELECT * FROM noticias ORDER BY data DESC";
+        $sql = "SELECT 
+                   noticias.id,
+                   noticias.titulo,
+                   noticias.data,
+                   usuarios.nome  AS autor
+                FROM noticias JOIN usuarios 
+                
+                ON  noticias.usuario_id = usuarios.id
+                
+                ORDER BY data DESC";
 
         $consulta = $this->conexao->query($sql);
 
