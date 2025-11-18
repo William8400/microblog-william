f474b6b61d2e360a1a670f58918d8dc6f91be58d
William8400 <wdonato02@gmail.com>
2025-11-18 17:25:19 -0300

    Update noticias.php

--- admin/noticias.php
+++ admin/noticias.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once "../src/Database/Conecta.php";
 require_once "../src/Helpers/Utils.php";
 require_once "../src/Services/NoticiaServico.php";
@@ -11,9 +11,8 @@ $noticiaServico = new NoticiaServico();
 
 try {
 	$noticias = $noticiaServico->buscar();
-	Utils::dump($noticias);
 } catch (Throwable $e) {
-	$erro = "Erro ao buscar notícias. <br>".$e->getMessage();
+	$erro = "Erro ao buscar notícias. <br>" . $e->getMessage();
 }
 
 
@@ -23,8 +22,8 @@ require_once "../includes/cabecalho-admin.php";
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
-		
-		<h2 class="text-center">Notícias <span class="badge bg-dark">X</span></h2>
+
+		<h2 class="text-center">Notícias <span class="badge bg-dark"><?= count($noticias) ?></span></h2>
 
 		<?php if ($erro): ?>
 			<p class="alert alert-danger text-center"> <?= $erro ?> </p>
@@ -32,17 +31,17 @@ require_once "../includes/cabecalho-admin.php";
 
 		<p class="text-center mt-5">
 			<a class="btn btn-primary" href="noticia-insere.php">
-			<i class="bi bi-plus-circle"></i>	
-			Inserir nova notícia</a>
+				<i class="bi bi-plus-circle"></i>
+				Inserir nova notícia</a>
 		</p>
-				
+
 		<div class="table-responsive">
-		
+
 			<table class="table table-hover">
 				<thead class="table-light">
 					<tr>
-                        <th>Título</th>
-                        <th>Data</th>				
+						<th>Título</th>
+						<th>Data</th>
 						<th>Autor</th>
 
 						<th class="text-center" colspan="2">Operações</th>
@@ -51,36 +50,37 @@ require_once "../includes/cabecalho-admin.php";
 
 				<tbody>
 
-
+				<?php foreach ($noticias as  $noticia): ?>
 					<tr>
-                        <td> Título... </td>
-                        <td> Data... </td>
-						<td> Autor... </td>
-						
+						<td> <?= $noticia['titulo'] ?> </td>
+						<td> <?= $noticia['data'] ?> </td>
+						<td> <?= $noticia['autor'] ?> </td>
+
 
 						<td class="text-center">
-							<a class="btn btn-warning" 
-							href="noticia-atualiza.php">
-							<i class="bi bi-pencil"></i> Atualizar
+							<a class="btn btn-warning"
+								href="noticia-atualiza.php?id=<?= $noticia['id'] ?>">
+								<i class="bi bi-pencil"></i> Atualizar
 							</a>
 						</td>
 						<td>
-							<a class="btn btn-danger excluir" 
-							href="noticia-exclui.php">
-							<i class="bi bi-trash"></i> Excluir
+							<a class="btn btn-danger excluir"
+								href="noticia-exclui.php?id=<?= $noticia['id'] ?>">
+								<i class="bi bi-trash"></i> Excluir
 							</a>
 						</td>
 					</tr>
 
-				</tbody>                
+				<?php endforeach; ?>
+
+				</tbody>
 			</table>
-	</div>
-		
+		</div>
+
 	</article>
 </div>
 
 
-<?php 
+<?php
 require_once "../includes/rodape-admin.php";
 ?>
-
