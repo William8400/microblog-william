0a8155f7a2aa928fa44d1b6e7aae07d523d8cf56
William8400 <wdonato02@gmail.com>
2025-11-19 16:32:26 -0300

    Update Utils.php

--- src/Helpers/Utils.php
+++ src/Helpers/Utils.php
@@ -66,6 +66,11 @@ class Utils
     public static function upload(?array $arquivo): void
     {
 
+        /* Validação inicial, verificando se: 
+        - não tem arquivo
+        - não existe alguma referência na área temporária
+        - não for um arquivo que possa/permita envio/upload    
+        */
         if (
             !$arquivo ||
             !isset($arquivo["tmp_name"]) ||
@@ -74,22 +79,33 @@ class Utils
             throw new Exception("Nenhum arquivo válido foi enviado.");
         }
 
+        // Definindo uma pasta no servidor/site para receber a imagem enviada 
         $pastaDeDestino = "../images/";
+
+        // Validação dos formatos de imagem
         $formatosPermitidos = ['image/jpeg', 'image/png', 'image/gif', 'image/svg+xml'];
+
+        // definindo um tamanho máximo pra imagem
         $tamanhoMaximo = 2 * 1024 * 1024; // 2MB
 
+
+        // Detectando o formato REAL do arquivo
         $formatoDoArquivoEnviado = mime_content_type($arquivo["tmp_name"]);
 
+        // Se o formato NÃO ESTIVER na lista de formatos permitidos 
         if (!in_array($formatoDoArquivoEnviado, $formatosPermitidos)) {
             throw new Exception("Apenas arquivos JPG, PNG, GIF e SVG são permitidos.");
         }
 
+        // Se o tamanho do arquivo for acima do máximo 
         if ($arquivo["size"] > $tamanhoMaximo) {
             throw new Exception("O arquivo é muito grande. Tamanho máximo: 2MB.");
         }
 
+        // Montando o nome/caminho do arquivo que será guardado na pasta imagens
         $nomeDoArquivo = $pastaDeDestino . basename($arquivo["name"]);
 
+        // Se não CONSEGUIR executar a função move_uploaded_file, lançar uma exceção
         if (!move_uploaded_file($arquivo["tmp_name"], $nomeDoArquivo)) {
             throw new Exception("Erro ao mover o arquivo. Código de erro: " . $arquivo["error"]);
         }
