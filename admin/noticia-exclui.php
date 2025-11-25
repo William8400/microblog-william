63d685a09db5581dbf82e5414346eb4d7bcef908
William8400 <wdonato02@gmail.com>
2025-11-25 16:07:31 -0300

    chamando o método excluir

--- admin/noticia-exclui.php
+++ admin/noticia-exclui.php
@@ -1,7 +1,25 @@
 <?php
+require_once "../src/Database/Conecta.php";
+require_once "../src/Models/Noticia.php";
+require_once "../src/Services/NoticiaServico.php";
 require_once "../src/Helpers/Utils.php";
 require_once "../src/Services/AutenticacaoServico.php";
 AutenticacaoServico::exijirLogin();
+
+$erro = null;
+
+$noticiaServico = new NoticiaServico();
+
+$id = Utils::sanitizar($_GET['id'], 'inteiro');
+
+if(!$id) Utils::redirecionarPara("noticias.php");
+
+try {
+	$noticiaServico->excluir($id, $_SESSION['id'], $_SESSION['tipo']);
+} catch (Throwable $e) {
+	$erro = "Erro ao excluir noticia. <br>".$e->getMessage();
+}
+
 
 require_once "../includes/cabecalho-admin.php";
 ?>
