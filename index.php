3f81e0554560f20a1822f80db4c604d9e5ab75c2
William8400 <wdonato02@gmail.com>
2025-11-26 14:45:31 -0300

    tirando um espaço do id

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ require_once "includes/cabecalho.php";
 
     <div class="col-md-6 my-1 px-md-1">
         <article class="card shadow-sm h-100">
-            <a href="noticia.php?id= <?= $noticia['id'] ?>" class="card-link">
+            <a href="noticia.php?id=<?= $noticia['id'] ?>" class="card-link">
                 <img src="images/<?=$noticia['imagem']?>" class="card-img-top" alt="Imagem de capa do card" >
                 <div class="card-body">
                     <h3 class="fs-4 card-title"><?= $noticia['titulo'] ?></h3>
